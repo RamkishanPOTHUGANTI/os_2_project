76f64863ea1c0bdb53d06a7e78bc214bbc753be5
r@MKishAn <35762247+RamkishanPOTHUGANTI@users.noreply.github.com>
2018-04-20 22:18:29 +0530

    Update libmem.hpp
    changes structs , added new functions

--- libmymem.hpp
+++ libmymem.hpp
@@ -20,35 +20,43 @@ enum BucketSize{  // Enum for indexing into the correct size
 };
 
 
-struct Slab {
-
-	int totalObj;
-	int freeObj;
-	bool bitmap[4096];
-	int initialize(){
-		totalObj=0;
-		freeObj=0;
-	}
-//	Bucket * bucket;
-
-	Slab * nextpointer;
-	Object * start_obj;
-
-};
-
-
 struct Object {
-	Slab * parentSlab;
-	void * memory;
+	void* parentSlab = NULL;
+	void * memory = NULL;
 	Object * nextpointer;
-
 };
 
+struct Slab {
+	int totalObj = 0 ; //4 bytes
+	int freeObj = 0; //4 bytes
+	int BucketSize = 0; //4 bytes
+	bool bitmap[4096]; //4096 bytes
+	void * bucket = NULL;
+	Slab * nextpointer;
+};
 struct Bucket {
 	size_t bucketSize;
 	Slab * slab;
 };
-
+void intialise_slab(Slab * s , int bs){
+	s->bucketSize = bs;
+	int used_space = 10;
+	s->totalObj = (64*1024 - B)/sizeof(Object);
+	s->freeObj = s->totalObj;
+	update_parent_bucket_in_Slab(s,&Bucket[bs]); 
+}
+void update_parent_slab_in_Object(Slab * s , Object * o){
+	o->parentSlab = (void *)s;
+}
+Slab * get_parent_slab_address(Object* o){
+	return (Slab *)(o->parentSlab);
+}
+void update_parent_bucket_in_Slab(Bucket * b , Slab * s){
+	s->bucket = (void *)b;
+}
+Bucket * get_parent_bucket_address(Slab* s){
+	return (Bucket *)(s->bucket);
+}
 
 Bucket Table[N];		// Table of type bucket
 
@@ -62,20 +70,30 @@ int deallocate_slab_chunk(void* p){
 	return munmap(p,SLAB_SIZE);
 }
 
-template <typename nodepointer>
-int insert(nodepointer a , nodepointer b){
-	if(a == nullptr){
-		//allocate space to b
-		a = b;
-		return 0;
+
+Slab* insert_slab_in_bucket(Slab * first_slab_in_bucket,int bucket_size){
+	if(first_slab_in_bucket == NULL){
+		Slab * slab_ ;
+		void * tempptr = allococate_slab_chunk();
+		if(tempptr == MAP_FAILED){
+			return nullptr;
+		}
+		slab_ = (Slab*)tempptr;  //use some dynmaci cast or static cast
+
+		first_slab_in_bucket = slab_;
+		return first_slab_in_bucket;
 	}
 	else{
-		nodepointer temp = a;
+	  Slab * slab_ ;
+	  void * tempptr = allococate_slab_chunk();
+	  if(tempptr == MAP_FAILED){
+		     return nullptr;
+	  }
+	  slab_ = (Slab*)tempptr;  //use some dynmaci cast or static cast
+		Slab * temp = first_slab_in_bucket;
 		while(temp->nextpointer == nullptr)temp = temp->nextpointer;
-		//allocate space to b
-		temp = b;
-		return 0;
+		temp->nextpointer = slab_;
+		return temp->nextpointer;
 	}
-	return 1;
+	return nullptr;
 }
-
